4df5f166ddfda95fe09af76c78a8a645749e0aa2
Aditya Sood <aditya.sood@pcc.edu>
2018-10-20 17:07:29 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,9 +1,11 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
 /**
  * created by Aditya Sood on 10/19/18
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World from Aditya!");
+        System.out.println(new Date());
     }
 }
