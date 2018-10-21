ae515934148b929fbf2a3d4774d728f90c9506d1
Aditya Sood <aditya.sood@pcc.edu>
2018-10-20 17:04:33 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,9 @@
 package edu.pcc.cis.helloworld;
-
+/**
+ * created by Aditya Sood on 10/19/18
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Aditya!");
+    }
 }
